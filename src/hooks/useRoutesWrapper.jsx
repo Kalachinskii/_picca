e652fa8d419de62a7c9867abc6aada7083bdb9b6
Vladislav <kalachinskii_vl@mail.ru>
2025-04-27 23:59:43 +0300

    fix

--- src/hooks/useRoutesWrapper.jsx
+++ src/hooks/useRoutesWrapper.jsx
@@ -3,7 +3,5 @@ import { routes } from "../utils/routes";
 
 // через use
 export function useRoutesWrapper() {
-    // +внешнии хуки и логика
-    // возвращяем результат а не верстку
     return useRoutes(routes);
 }
