2929352c91fe178ad3ef8c6636505cb890ae15ab
Vladislav <kalachinskii_vl@mail.ru>
2025-06-11 22:16:25 +0300

    fix: id на ind - Category.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,35 +2,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { setCategory } from "../store/slices/filterSlice";
 
 function Categories() {
-    const categories = [
-        "Все",
-        "Мясные",
-        "Вегетарианская",
-        "Гриль",
-        "Острые",
-        "Закрытые",
-    ];
-    const activeCategory = useSelector((state) => state.filter.category);
-    const dispatch = useDispatch();
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const activeCategory = useSelector((state) => state.filter.category);
+  const dispatch = useDispatch();
 
-    return (
-        <div className="categories">
-            <ul>
-                {categories.map((category, id) => (
-                    <li
-                        key={id}
-                        className={
-                            activeCategory === id ? "activeCategory" : ""
-                        }
-                        // dispatch - передаём action
-                        onClick={() => dispatch(setCategory(id))}
-                    >
-                        {category}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((category, ind) => (
+          <li
+            key={ind}
+            className={activeCategory === ind ? "activeCategory" : ""}
+            // dispatch - передаём action
+            onClick={() => dispatch(setCategory(ind))}
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Categories;
