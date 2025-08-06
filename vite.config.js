c7037f5bc888b0e3d43bc072dfff702452e4eee8
Vladislav <kalachinskii_vl@mail.ru>
2025-08-06 20:17:41 +0300

    fix: исправление ошибки dev сборки

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",
+  server: {
+    port: 3000,
   },
   resolve: {
     alias: {
